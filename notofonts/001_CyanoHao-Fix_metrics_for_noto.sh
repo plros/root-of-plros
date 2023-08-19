cbe395e16ab880b7669586133e46bc910a3aca57
Whot1966 <Whot1966@users.noreply.github.com>
2023-08-19 15:15:49 +0000

    Import necesarry scripts * Add some files to .gitignore * Fix metrics script is from https://github.com/CyanoHao/magisk-notosans
    Co-authored-by: CyanoHao <c@cyano.cn>

---
+++ notofonts/001_CyanoHao-Fix_metrics_for_noto.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cp NotoSans\[wdth\,wght\].ttf NotoSans-RegularVF.ttf
+cp NotoSans-Italic\[wdth\,wght\].ttf NotoSans-ItalicVF.ttf
+for w in Thin Light Regular Medium Bold Black ThinItalic LightItalic Italic MediumItalic BoldItalic BlackItalic RegularVF ItalicVF; do
+  ttx -t 'head' -t 'hhea' -o $w.ttx NotoSans-$w.ttf
+  sed -i 's/yMin value="-[0-9]\+"/yMin value="-271"/; s/yMax value="[0-9]\+"/yMax value="1056"/; s/ascent value="[0-9]\+"/ascent value="928"/; s/descent value="-[0-9]\+"/descent value="-244"/' $w.ttx
+  ttx -m NotoSans-$w.ttf -o ResultNoto-$w.ttf $w.ttx
+  rm $w.ttx
+done
+for w in Light Regular Medium Bold LightItalic Italic MediumItalic BoldItalic; do
+  if [[ $w == Regular ]]; then
+    notosansfilename=NotoSans-Condensed.ttf
+  else
+    notosansfilename=NotoSans-Condensed$w.ttf
+  fi
+  ttx -t 'head' -t 'hhea' -o $w.ttx $notosansfilename
+  sed -i 's/yMin value="-[0-9]\+"/yMin value="-271"/; s/yMax value="[0-9]\+"/yMax value="1056"/; s/ascent value="[0-9]\+"/ascent value="928"/; s/descent value="-[0-9]\+"/descent value="-244"/' $w.ttx
+  ttx -m $notosansfilename -o ResultNotoCondensed-$w.ttf $w.ttx
+  rm $w.ttx
+done
