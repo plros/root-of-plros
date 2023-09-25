887abc9e8aebbf72915d9e830a869a8fe4b431d4
Whot1966 <Whot1966@users.noreply.github.com>
2023-09-25 06:42:08 +0000

    Update Cromite path

--- manyscripts/Update_Cromite.sh
+++ manyscripts/Update_Cromite.sh
@@ -5,13 +5,13 @@ cromite_versioning_for_me_only="$(curl -sL https://api.github.com/repos/uazo/cro
 echo "Getting latest release ..."
 wget $(curl -L -s https://api.github.com/repos/uazo/cromite/releases/latest | grep -o -E "https://(.*)arm64_ChromePublic(.*).apk")
 
-if ! [ -d "$HOME/android_vendor_plros" ]; then
-echo "android_vendor_plros not found! Cloning..."
-git clone https://github.com/plros/android_vendor_plros ~/android_vendor_plros --depth=1
+if ! [ -d "$HOME/vendor_plros_prebuilt_apps_Cromite" ]; then
+echo "vendor_plros_prebuilt_apps_Cromite not found! Cloning..."
+git clone https://gitlab.com/plros-lab/vendor_plros_prebuilt_apps_Cromite ~/vendor_plros_prebuilt_apps_Cromite --depth=1
 fi
 
 echo "Moving ..."
-mv arm64_ChromePublic.apk ~/android_vendor_plros/prebuilt/apps/Cromite/arm64_ChromePublic.apk
+mv arm64_ChromePublic.apk ~/vendor_plros_prebuilt_apps_Cromite/arm64_ChromePublic.apk
 rm -rf arm64_ChromePublic.apk
 echo "Done! This is your commit message:"
 echo "Update Cromite as $date_for_now_and_for_this_script_only and $cromite_versioning_for_me_only"
