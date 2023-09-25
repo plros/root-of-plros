f86664864138f35e39b087ede4e6d3faf1b1b968
kdrag0n <danny@kdrag0n.dev>
2023-09-25 05:00:00 +0000

    Add script for spoof Noto Sans name

--- notofonts/001_CyanoHao-Fix_metrics_for_noto.sh
+++ notofonts/001_CyanoHao-Fix_metrics_for_noto.sh
@@ -3,8 +3,9 @@
 cp NotoSans\[wdth\,wght\].ttf NotoSans-RegularVF.ttf
 cp NotoSans-Italic\[wdth\,wght\].ttf NotoSans-ItalicVF.ttf
 for w in Thin Light Regular Medium Bold Black ThinItalic LightItalic Italic MediumItalic BoldItalic BlackItalic RegularVF ItalicVF; do
-  ttx -t 'head' -t 'hhea' -o $w.ttx NotoSans-$w.ttf
+  ttx -t 'head' -t 'hhea' -t 'name' -o $w.ttx NotoSans-$w.ttf
   sed -i 's/yMin value="-[0-9]\+"/yMin value="-271"/; s/yMax value="[0-9]\+"/yMax value="1056"/; s/ascent value="[0-9]\+"/ascent value="928"/; s/descent value="-[0-9]\+"/descent value="-244"/' $w.ttx
+  sed -i 's/Noto Sans/Roboto/g' $w.ttx
   ttx -m NotoSans-$w.ttf -o ResultNoto-$w.ttf $w.ttx
   rm $w.ttx
 done
@@ -14,8 +15,14 @@ for w in Light Regular Medium Bold LightItalic Italic MediumItalic BoldItalic; d
   else
     notosansfilename=NotoSans-Condensed$w.ttf
   fi
-  ttx -t 'head' -t 'hhea' -o $w.ttx $notosansfilename
+  ttx -t 'head' -t 'hhea' -t 'name' -o $w.ttx $notosansfilename
   sed -i 's/yMin value="-[0-9]\+"/yMin value="-271"/; s/yMax value="[0-9]\+"/yMax value="1056"/; s/ascent value="[0-9]\+"/ascent value="928"/; s/descent value="-[0-9]\+"/descent value="-244"/' $w.ttx
+  sed -i 's/Noto Sans/Roboto/g' $w.ttx
   ttx -m $notosansfilename -o ResultNotoCondensed-$w.ttf $w.ttx
   rm $w.ttx
 done
+cp ResultNoto-Regular.ttf NotoStatic-Regular.ttf
+ttx -t 'name' -o NotoStatic-Regular.ttf.ttx NotoStatic-Regular.ttf 
+sed -i 's/Roboto/RobotoStatic/g' NotoStatic-Regular.ttf.ttx
+ttx -m NotoStatic-Regular.ttf -o ResultNotoStatic-Regular.ttf NotoStatic-Regular.ttf.ttx
+rm NotoStatic-Regular.ttf.ttx NotoStatic-Regular.ttf
--- notofonts/002_Move_results_to_respective_folder.sh
+++ notofonts/002_Move_results_to_respective_folder.sh
@@ -16,6 +16,6 @@ cp ResultNoto-$w.ttf ~/android_vendor_plros/fonts/NotoSans-$w.ttf
 done
 
 cp ResultNoto-RegularVF.ttf ~/android_external_roboto-fonts/Roboto-Regular.ttf 
-cp ResultNoto-Regular.ttf ~/android_external_roboto-fonts/RobotoStatic-Regular.ttf 
+cp ResultNotoStatic-Regular.ttf ~/android_external_roboto-fonts/RobotoStatic-Regular.ttf 
 echo "Finish!"
 echo "Please push the changes and re-sync your repo!"
