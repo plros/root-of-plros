c0f8a05fd924bd8b0f78c870f65d1344f59532f9
Whot1966 <Whot1966@users.noreply.github.com>
2023-10-13 07:56:36 +0700

    Update scripts path

--- notofonts/002_Move_results_to_respective_folder.sh
+++ notofonts/002_Move_results_to_respective_folder.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-if ! [ -d "$HOME/android_vendor_plros" ]; then
-echo "android_vendor_plros not found! Cloning..."
-git clone https://github.com/plros/android_vendor_plros ~/android_vendor_plros --depth=1
+if ! [ -d "$HOME/vendor_plros" ]; then
+echo "vendor_plros not found! Cloning..."
+git clone https://github.com/plros/android_vendor_plros ~/vendor_plros --depth=1
 fi
 
-if ! [ -d "$HOME/android_external_roboto-fonts" ]; then
-echo "android_external_roboto-fonts not found! Cloning..."
-git clone https://github.com/Whot1966/android_external_roboto-fonts ~/android_external_roboto-fonts --depth=1
+if ! [ -d "$HOME/external_roboto-fonts" ]; then
+echo "external_roboto-fonts not found! Cloning..."
+git clone https://github.com/plros/external_roboto-fonts ~/external_roboto-fonts --depth=1
 fi
 
 echo "Start copying ..."
 for w in Light Regular Medium LightItalic Italic MediumItalic; do
-cp ResultNoto-$w.ttf ~/android_vendor_plros/fonts/NotoSans-$w.ttf 
+cp ResultNoto-$w.ttf ~/vendor_plros/fonts/NotoSans-$w.ttf 
 done
 
-cp ResultNoto-RegularVF.ttf ~/android_external_roboto-fonts/Roboto-Regular.ttf 
-cp ResultNotoStatic-Regular.ttf ~/android_external_roboto-fonts/RobotoStatic-Regular.ttf 
+cp ResultNoto-RegularVF.ttf ~/external_roboto-fonts/Roboto-Regular.ttf 
+cp ResultNotoStatic-Regular.ttf ~/external_roboto-fonts/RobotoStatic-Regular.ttf 
 echo "Finish!"
 echo "Please push the changes and re-sync your repo!"
